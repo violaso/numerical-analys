39f9d453984bf54d67dd8e73294590b48018afa5
Viola Söderlund <violaso@kth.se>
2020-03-23 17:35:55 +0100

    1 done!

--- laboration_1/nonlinear_scalar_equation/nonlinear_scalar_equation.m
+++ laboration_1/nonlinear_scalar_equation/nonlinear_scalar_equation.m
@@ -21,78 +21,101 @@ hold off
 % b. Undersöker vilka av funktionens rötter som kan bestämmas av
 %    fixpunktsiterationen next_x(x*) = x*, och beräknar sedan rötterna.
 
+disp('------------------------------');
 disp('--- b. Fixpunktiterationer ---');
+disp('------------------------------');
 
 next_x = @(x) (f(x) / 9 + x);
 Dnext_x = @(x) (Df(x) / 9 + 1);
 
     % Fall 1
-    disp('Fall 1');
+    disp('--- Fall 1 ---');
     
     xn = -0.6;
     x0 = xn
     
-    [convergens, num_iterations, solution] = get_convergens(next_x, xn);
+    [convergens, solution] = get_convergens(next_x, xn);
     
     if convergens
-        get_is_quadratic(Dnext_x, solution);
+        if Dnext_x(solution) == 0
+            disp('----> Iterationen konvergerar kvadratiskt.');
+        else 
+            disp('----> Iterationen konvergerar inte kvadratiskt.');
+        end
     end
         
     % Fall 2  
-    disp('Fall 2');
+    disp('--- Fall 2 ---');
     
     xn = 0.48;
     xo = xn
     
-    [convergens, num_iterations, solution] = get_convergens(next_x, xn);
+    [convergens, solution] = get_convergens(next_x, xn);
     
     if convergens
-        get_is_quadratic(Dnext_x, solution);
+        if Dnext_x(solution) == 0
+            disp('----> Iterationen konvergerar kvadratiskt.');
+        else 
+            disp('----> Iterationen konvergerar inte kvadratiskt.');
+        end
     end
     
     disp('--> Correction: Andra nollvärdet är en falsk lösning! Endast första nollvärdet är sann.')
 
 % c. Beräknar rötterna med Newtons metod.
 
-disp('--- b. Newtons metod ---')
+disp('------------------------');
+disp('--- b. Newtons metod ---');
+disp('------------------------');
 
-next_x = @(x) (xn - f(x) / Df(x));
+next_x = @(x) (x - f(x) / Df(x));
 
     % Fall 1
-    disp('Fall 1');
+    disp('--- Fall 1 ---');
     
     xn = -0.73;
     x0 = xn
     
-    %[num_iterations, solution] = calculate_solution(next_x, xn, next_x(xn));
+    solution = calculate_solution(next_x, xn, next_x(xn));
     
-    disp('--> Fixpunktsiterationen konvergerar mot första nollvärdet x* med givet antal iterationer.');
+    if Df(solution) ~= 0
+        disp('----> Iterationen konvergerar kvadratiskt.');
+    else 
+        disp('----> Iterationen konvergerar inte kvadratiskt.');
+    end
         
     % Fall 2
-    disp('Fall 2');
+    disp('--- Fall 2 ---');
     
-    xn = 0.5;
+    xn = 0.47;
     x0 = xn
     
-    %[num_iterations, solution] = calculate_solution(next_x, xn, next_x(xn));
+    solution = calculate_solution(next_x, xn, next_x(xn));
     
-    disp('--> Fixpunktsiterationen konvergerar mot andra nollvärdet x* med givet antal iterationer.');
-    
-% Checks if iterations are quadratic by examining it's derivitive function.
-function is_quadratic = get_is_quadratic(Dnext_x, xn)
-    if Dnext_x(xn) == 0
+    if Df(solution) ~= 0
         disp('----> Iterationen konvergerar kvadratiskt.');
-        
-        is_quadratic = true;
-    else
+    else 
         disp('----> Iterationen konvergerar inte kvadratiskt.');
-        
-        is_quadratic = false;
     end
-end
+    
+    % Comparison
+    
+    disp('Fixpunktsiterationen (b) konvergerade långsammare än Newtons funktion (c), då Newtons metod har en högre konvergensordning.');
+    
+% d. Beskriver Newtons metods konvergensordning.
 
-% Checks if iterations are convergent and if true, calculates the solution.
-function [convergens, num_iterations, solution] = get_convergens(next_x, xn)
+disp('-------------------------------------------');
+disp('--- d. Newtons metods konvergensordning ---');
+disp('-------------------------------------------');
+
+disp('Newtons metod har konvergensordning 2, då fixpunktiterationen konvergerar linjärt. Observera fall 1. Fixpunktsiterationen itererar 19 gånger, medan Newton itererar 4 gånger.');
+
+fixed_point_iterations = 19
+newton_method_iterations = 4
+
+newton_method_convergence_order = floor(log(fixed_point_iterations) / log(newton_method_iterations))
+
+function [convergens, solution] = get_convergens(next_x, xn)
        
     % Check convergens
     
@@ -100,17 +123,14 @@ function [convergens, num_iterations, solution] = get_convergens(next_x, xn)
         
     if abs(xn_incremented) > 1
         disp('Fixpunksiterationen divergerar runt xn = x0 ? x*.');
-        xn
             
         convergens = false;
     elseif abs(xn_incremented) < 0.1
         disp('Fixpunktsiterationen konvergerar snabbt runt xn = x0 ? x*.');
-        xn
             
         convergens = true;
     else
-        disp('Fixpunktsiterationen konvergerar runt xn = x0 ? x*.');
-        xn
+        disp('Fixpunktsiterationen konvergerar runt xn = x0 ? x*.')
 
         convergens = true;
     end
@@ -121,40 +141,28 @@ function [convergens, num_iterations, solution] = get_convergens(next_x, xn)
     solution = Inf;
         
     if convergens
-            
-        [num_iterations, solution] = calculate_solution(next_x, xn, xn_incremented)
+        solution = calculate_solution(next_x, xn, xn_incremented);
     end
 end
 
-% Iterates a soluition given values that converge.
-function [num_iterations, solution] = calculate_solution(next_x, xn, xn_incremented)
-
-    max_iterations = 100;
+function solution = calculate_solution(next_x, xn, xn_incremented)
     
     % Calculate solution
     
     iterations = xn;
-    
-    num_iterations = -1;
     solution = Inf;
-    
-    disp('Iterating...')
         
-    count = 1;
-    while xn_incremented ~= xn && count < max_iterations
+    while xn_incremented ~= xn && abs(xn_incremented - xn) > eps
         xn = xn_incremented;
         xn_incremented = next_x(xn);
-                
-        count = count + 1;
         
-        iterations = [ iterations;xn ];
+        iterations = [ iterations; xn ];
     end
     
-    num_iterations = count;
     solution = xn;
     
     iterations
-    num_iterations
+    num_iterations = length(iterations)
        
     solution
 end
